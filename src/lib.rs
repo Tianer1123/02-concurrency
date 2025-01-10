38be853196d46afba73d9050da840f03d9a772ed
Tianer1123 <q2719833@126.com>
2025-01-11 07:42:13 +0800

    feature: support matrix multiply

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod matrix;
+
+pub use matrix::{multiply, Matrix};
+
+impl<T> Matrix<T> {
+    pub fn new(data: impl Into<Vec<T>>, row: usize, col: usize) -> Self {
+        Self {
+            data: data.into(),
+            row,
+            col,
+        }
+    }
+}
