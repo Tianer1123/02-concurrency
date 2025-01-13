d922a5a95525d9334e6c28eb7288310dcad6bc83
Tianer1123 <q2719833@126.com>
2025-01-13 08:50:15 +0800

    feature: support rw lock & display

--- src/metrics.rs
+++ src/metrics.rs
@@ -3,25 +3,26 @@
 
 use anyhow::anyhow;
 use anyhow::Result;
+use std::fmt;
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
 };
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, u64>>>,
+    data: Arc<RwLock<HashMap<String, u64>>>,
 }
 
 impl Metrics {
     pub fn new() -> Metrics {
         Metrics {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let count = data.entry(key.into()).or_insert(0);
         *count += 1;
         Ok(())
@@ -30,7 +31,7 @@ impl Metrics {
     pub fn snapshot(&self) -> Result<HashMap<String, u64>> {
         Ok(self
             .data
-            .lock()
+            .read()
             .map_err(|e| anyhow!(e.to_string()))?
             .clone())
     }
@@ -39,5 +40,15 @@ impl Metrics {
 impl Default for Metrics {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl fmt::Display for Metrics {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let data = self.data.read().map_err(|_e| fmt::Error {})?;
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
     }
 }
