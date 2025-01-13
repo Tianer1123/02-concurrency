2da0770411c97635e2115d890f9c9673582d9e2e
Tianer1123 <q2719833@126.com>
2025-01-13 08:37:25 +0800

    fix: do not use unwrap

---
+++ src/metrics.rs
@@ -0,0 +1,43 @@
+// metrics data structure;
+// 基本功能: inc/dec/snapshot
+
+use anyhow::anyhow;
+use anyhow::Result;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+#[derive(Debug, Clone)]
+pub struct Metrics {
+    data: Arc<Mutex<HashMap<String, u64>>>,
+}
+
+impl Metrics {
+    pub fn new() -> Metrics {
+        Metrics {
+            data: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub fn inc(&self, key: impl Into<String>) -> Result<()> {
+        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let count = data.entry(key.into()).or_insert(0);
+        *count += 1;
+        Ok(())
+    }
+
+    pub fn snapshot(&self) -> Result<HashMap<String, u64>> {
+        Ok(self
+            .data
+            .lock()
+            .map_err(|e| anyhow!(e.to_string()))?
+            .clone())
+    }
+}
+
+impl Default for Metrics {
+    fn default() -> Self {
+        Self::new()
+    }
+}
